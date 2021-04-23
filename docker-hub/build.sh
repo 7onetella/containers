a3180e0abdffd4a45c8d417245aebafd3310302a
7onetella <7onetella@gmail.com>
2021-04-23 01:40:22 -0400

    updating docker-hub to use tls

--- docker-hub/build.sh
+++ docker-hub/build.sh
@@ -4,7 +4,6 @@ version=${1}
 
 docker build -t docker-registry.7onetella.net/7onetella/docker-hub:"${version}" .
 
-# docker push docker-registry.7onetella.net/7onetella/docker-hub:"${version}"
 docker push docker-registry.7onetella.net/7onetella/docker-hub:"${version}"
 
 vag docker deploy docker-hub-dev:"${version}"
