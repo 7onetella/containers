e94c7c97695362414ed0795b30ab77ebc7749f7f
Seven Tella <7onetella@gmail.com>
2021-05-20 21:48:39 +0000

    updating builder

--- builder/builder-build.sh
+++ builder/builder-build.sh
@@ -14,4 +14,6 @@ docker build -t docker-registry.7onetella.net/7onetella/${service}:"${version}" 
 
 docker push docker-registry.7onetella.net/7onetella/${service}:"${version}"
 
+export DOCKER_REGISTRY=docker-registry.7onetella.net
+
 vag docker deploy ${service}-${group}:"${version}"
