7c190dad31bf38da79a916d7600b75d96e5813b2
7onetella <7onetella@gmail.com>
2021-05-20 02:24:41 -0400

    updating service name parsing

--- base/builder/entrypoint.sh
+++ base/builder/entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-export SSH_PORT=$(curl -s "http://consul.7onetella.net:8500/v1/health/service/builder-dev-builder-service?dc=dc1" | jq -r ".[0].Service.TaggedAddresses.lan_ipv4.Port")
-export HOST_IP=$(curl -s "http://consul.7onetella.net:8500/v1/health/service/builder-dev-builder-service?dc=dc1" | jq -r ".[0].Service.Address")
+export SSH_PORT=$(curl -s "http://consul.7onetella.net:8500/v1/health/service/builder-dev-container?dc=dc1" | jq -r ".[0].Service.TaggedAddresses.lan_ipv4.Port")
+export HOST_IP=$(curl -s "http://consul.7onetella.net:8500/v1/health/service/builder-dev-container?dc=dc1" | jq -r ".[0].Service.Address")
 export UUID=$(uuid)
 export TERM=xterm
 
