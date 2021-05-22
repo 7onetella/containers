d57c33583311222434106061babaca7f1ccdc4cf
7onetella <7onetella@gmail.com>
2021-05-22 18:34:55 -0400

    adding ember and gh to runtime install script

--- codeserver/entrypoint.sh
+++ codeserver/entrypoint.sh
@@ -10,4 +10,6 @@ sudo chown root:docker /var/run/docker.sock
 
 nohup /home/coder/.local/bin/github_checkout_all.sh &
 
+nohup /home/coder/.local/bin/runtime_install.sh &
+
 dumb-init /usr/bin/code-server --bind-addr 0.0.0.0:8080 /home/coder/workspace
