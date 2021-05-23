5ebb8db8eae4f6b73e0b90566704f1cb33416236
7onetella <7onetella@gmail.com>
2021-05-23 18:28:18 -0400

    fixing python plugin for code server

--- codeserver-base/build.sh
+++ codeserver-base/build.sh
@@ -4,8 +4,12 @@ set -x
 
 service=codeserver-base
 
-version=1.0.0
+version=1.0.1
 
-docker build -t docker-registry.7onetella.net/7onetella/${service}:"${version}" .
+docker build --no-cache -t docker-registry.7onetella.net/7onetella/${service}:"${version}" .
 
 docker push docker-registry.7onetella.net/7onetella/${service}:"${version}"
+
+
+# chnagelog
+# 1.0.1 - reverting code server back to 3.10.0 to fix python plugin click through issue
