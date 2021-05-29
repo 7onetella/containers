1510e20f92dac32246aeb7ced4a5937bfe086121
teachmecoding <7onetella@gmail.com>
2021-05-29 00:34:14 +0000

    updating messaging

--- codeserver/create_user.sh
+++ codeserver/create_user.sh
@@ -10,14 +10,23 @@ password=$2
 
 email=$3
 
+echo creating user $username 
 vag cx add-user $username "${password}" $email
 
+echo
+echo creating user repo /$username/project
 vag cx add-user-repo $username git@git.curiosityworks.org/$username/project
 
+echo
+echo adding user ide vscode
 vag cx add-user-ide  $username vscode
 
+echo
+echo adding /$username/project to ide instance
 vag cx add-ide-repo  $username vscode git@git.curiosityworks.org/$username/project
 
+echo
+echo generating private and public key
 rm -f ./id_rsa_${username} && ssh-keygen -t rsa -N '' -q -f ./id_rsa_${username}
 
 cat ./id_rsa_${username}     | vag cx user-private-key ${username}
@@ -26,10 +35,18 @@ cat ./id_rsa_${username}.pub | vag cx user-public-key ${username}
 
 # --------------------------------------------------------------------------
 
+echo
+echo adding user to gitea
 vag gitea add-user ${username} "${password}" $email
 
+echo
+echo creating repo project in gitea
 vag gitea create-user-repo ${username} project
 
+echo
+echo adding user\'s public key to gitea
 cat ./id_rsa_${username}.pub | vag gitea create-public-key ${username}
 
+echo
+echo deleting generated private and public keys
 rm -f ./id_rsa_${username} ./id_rsa_${username}.pub
--- codeserver/delete-user.sh
+++ codeserver/delete-user.sh
@@ -2,12 +2,18 @@
 
 clear
 
-set -x
+# set -x
 
 username=$1
 
+echo 
+echo deleting user ${username} from user database
 vag cx delete-user ${username}
 
+echo
+echo deleting user repo /${username}/project from gitea
 vag gitea delete-user-repo ${username} project
 
+echo
+echo deleting gitea user account for ${username}
 vag gitea delete-user ${username}
