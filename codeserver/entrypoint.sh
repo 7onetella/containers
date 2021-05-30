fea9ec23fce21566347f43534118f59e07d9d2f0
teachmecoding <7onetella@gmail.com>
2021-05-30 10:31:40 +0000

    adding gotty

--- codeserver/entrypoint.sh
+++ codeserver/entrypoint.sh
@@ -8,10 +8,10 @@ sudo /etc/init.d/ssh restart
 
 sudo chown root:docker /var/run/docker.sock
 
-/home/coder/.local/bin/git_clone.sh &
+nohup /home/coder/.local/bin/git_clone.sh &
 
-/home/coder/.local/bin/runtime_install.sh &
+nohup /home/coder/.local/bin/runtime_install.sh &
 
-/home/coder/.local/bin/gotty.sh &
+nohup /home/coder/.local/bin/gotty.sh &
 
 dumb-init /usr/bin/code-server --bind-addr 0.0.0.0:9991 /home/coder/workspace
