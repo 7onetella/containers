2cd55b94a5b0bd8969d0cdadad0c6c1d7a26a8f8
teachmecoding <7onetella@gmail.com>
2021-06-02 16:14:33 +0000

    renaming builder to tools

--- tools/build.sh
+++ tools/build.sh
@@ -4,16 +4,14 @@ set -x
 
 languages_version=${1:-1.0.1}
 
+version=${2:-1.0.1}
+
 cp ~/.ssh/authorized_keys .ssh/
 
 service=builder
 
 group=backend
 
-version=$(vag docker version patch ${service}-${group})
-
 docker build --build-arg LANGUAGES_VERION=${languages_version} -t docker-registry.7onetella.net/7onetella/${service}:"${version}" .
 
 docker push docker-registry.7onetella.net/7onetella/${service}:"${version}"
-
-vag docker deploy docker-registry.7onetella.net/7onetella/${service}-${group}:"${version}"
