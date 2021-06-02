dbf2ba5fa7e92e1bf80c96de909b522f47a385f1
teachmecoding <7onetella@gmail.com>
2021-06-02 20:16:33 +0000

    fixing

--- languages/build.sh
+++ languages/build.sh
@@ -12,6 +12,6 @@ base_version=${1:-1.0.0}
 
 version=$(../next_tag.sh 7onetella/languages)
 
-docker build --build-arg BASE_VERION=${base_version} -t docker-registry.7onetella.net/7onetella/${repo}:"${version}" .
+docker build --build-arg BASE_VERSION=${base_version} -t docker-registry.7onetella.net/7onetella/${repo}:"${version}" .
 
 docker push docker-registry.7onetella.net/7onetella/${repo}:"${version}"
